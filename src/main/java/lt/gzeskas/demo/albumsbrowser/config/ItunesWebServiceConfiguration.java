0419cf2385e49245000303929cd8ec3fb3303d12
Gzegoz Jurgo <gzegoz.jurgo@cujo.com>
2020-02-26 15:44:49 +0200

    Added top5 albums cacing

--- src/main/java/lt/gzeskas/demo/albumsbrowser/config/ItunesWebServiceConfiguration.java
+++ src/main/java/lt/gzeskas/demo/albumsbrowser/config/ItunesWebServiceConfiguration.java
@@ -1,10 +1,12 @@
 package lt.gzeskas.demo.albumsbrowser.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lt.gzeskas.demo.albumsbrowser.services.cache.CachingAlbumSearchService;
 import lt.gzeskas.demo.albumsbrowser.services.itunes.ItunesWebService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
@@ -21,6 +23,13 @@ public class ItunesWebServiceConfiguration {
     @Bean
     ItunesWebService itunesWebService(WebClient webClient, ObjectMapper objectMapper) {
         return new ItunesWebService(webClient, objectMapper);
+    }
+
+    @Bean
+    @Primary
+    CachingAlbumSearchService cachingAlbumSearchService(ItunesWebService itunesWebService,
+                                                        @Value("${itunes-service.requestLimitPerHour}") long requestLimitPerHour) {
+        return new CachingAlbumSearchService(itunesWebService, requestLimitPerHour);
     }
 
 }
